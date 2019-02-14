0ef374dce4a13681f1bab8b4d5185a9c5a432803
Nick <freezydrag@gmail.com>
2019-02-14 12:16:15 -0800

    IntroLab Assignment

---
+++ Lab/IntroLab/helloWorld/main.cpp
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: nick
+ *
+ * Created on February 14, 2019, 11:59 AM
+ */
+
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    cout << "Hello World!";
+    return 0;
+}
+
